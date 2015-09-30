79bff2a40616ae5617377364c96a41bf6efb5471
unknown <toomaste@V-215-02.edu.wks.metropolia.fi>
2015-09-30 10:25:33 +0300

    Spawn position is now random

--- Assets/Scripti/SpawnEnemy.cs
+++ Assets/Scripti/SpawnEnemy.cs
@@ -7,10 +7,13 @@ public class SpawnEnemy : MonoBehaviour
 	public GameObject enemy;                		// The enemy prefab to be spawned.
 	public float spawnTime = 8f;            		// How long between each spawn. In seconds
 	public Transform[] spawnPoints;         		// An array of the spawn points this enemy can spawn from.
-	
-	
+	public float delta = 0.5f;
+	float minX, maxX, minY, maxY;
+	int spawnCheckPositionsMaxTries = 50; // Max tries 
+
 	void Start ()
 	{
+		this.GetMinMaxWallsPositions ();
 		playerHealth = GameObject.Find(GlobalVars.playerTankName).GetComponent<PlayerHealth> ();
 		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
 		InvokeRepeating ("Spawn", spawnTime, spawnTime);
@@ -32,8 +35,52 @@ public class SpawnEnemy : MonoBehaviour
 		
 		// Find a random index between zero and one less than the number of spawn points.
 		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
 		
 		// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-		Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		//Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		Instantiate (enemy, GetSpawnPointVector(), spawnPoints[spawnPointIndex].rotation);
 	}
+
+	Vector3 GetSpawnPointVector() 
+	{
+		int currentTry = 0;
+
+		while(currentTry != spawnCheckPositionsMaxTries) 
+		{
+			currentTry++;
+			float x = Random.Range (minX, maxX);
+			float y = Random.Range (minY, maxY);
+			Vector3 temp = new Vector3(x, y, 0);
+
+			Collider[] hitColliders = Physics.OverlapSphere(temp, 2);
+			if (hitColliders.Length == 0) // Something on the way
+			{
+				return temp;
+			}
+		}
+
+		// Return current position if max tries exceeded
+		return new Vector3 (this.transform.position.x, this.transform.position.y, 0);
+	}
+
+	void GetMinMaxWallsPositions() 
+	{
+		GameObject[] metallWallObjects = GameObject.FindGameObjectsWithTag ("MetalWall");
+		foreach (GameObject mw in metallWallObjects)
+		{
+			float x = mw.transform.position.x;
+			float y = mw.transform.position.y;
+
+			if (x > maxX)
+				maxX = x;
+			if (x < minX)
+				minX = x;
+			if (y > maxY)
+				maxY = y;
+			if (y < minY)
+				minY = y;
+		}
+	}
+
 }
